feec109d3d17bb1b967ccea88206befc7a9fc234
Mr. Wheels <diego.zamarron0723@outlook.com>
2025-03-01 17:26:12 -0600

    Revert "closer"
    This reverts commit 4292025eda5a3eb81a1910986620fb16bd6916a7.

--- app/routes/autocomplete.py
+++ app/routes/autocomplete.py
@@ -1,10 +1,5 @@
 from flask import Blueprint, request, jsonify
 from app.utils.cache import get_tracker
-import logging
-import threading
-import time
-
-logger = logging.getLogger(__name__)
 
 autocomplete_bp = Blueprint('autocomplete', __name__)
 
@@ -13,26 +8,10 @@ def autocomplete():
     """
     Provide autocomplete suggestions for item names based on query input.
     """
-    logger.info("Autocomplete route started")
     query = request.args.get('query', '').lower()
-    logger.info(f"Autocomplete query: {query}")
     tracker = get_tracker()
-
-    lock_acquired = False
-    try:
-        lock_acquired = tracker.lock.acquire(timeout=5)  # Attempt to acquire lock with timeout
-        if lock_acquired:
-            suggestions = [
-                item_id for item_id in tracker.data.keys() if query in item_id.lower()
-            ][:10]
-            logger.info("Autocomplete route finished successfully")
-            return jsonify(suggestions)
-        else:
-            logger.error("Failed to acquire lock within timeout")
-            return jsonify([]), 500  # Return an error if lock not acquired
-    except Exception as e:
-        logger.exception("Exception in autocomplete route")
-        return jsonify([]), 500
-    finally:
-        if lock_acquired:
-            tracker.lock.release()
+    with tracker.lock:
+        suggestions = [
+            item_id for item_id in tracker.data.keys() if query in item_id.lower()
+        ][:10]
+    return jsonify(suggestions)
